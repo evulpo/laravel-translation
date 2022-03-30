9c3ae93d966ec4562b43ada5692a56e51848024e
georg@evulpo.com <55834338+GeorgBaseltETH@users.noreply.github.com>
2022-03-30 11:33:38 +0200

    Update TranslationFactory.php

--- database/factories/TranslationFactory.php
+++ database/factories/TranslationFactory.php
@@ -6,9 +6,6 @@ use JoeDixon\Translation\Translation;
 
 $factory->define(Translation::class, function (Generator $faker) {
     return [
-        'language_id' => function () {
-            return factory(Language::class)->create()->id;
-        },
         'group' => $faker->word,
         'key' => $faker->word,
         'value' => $faker->sentence,
@@ -17,9 +14,6 @@ $factory->define(Translation::class, function (Generator $faker) {
 
 $factory->state(Translation::class, 'group', function (Generator $faker) {
     return [
-        'language_id' => function () {
-            return factory(Language::class)->create()->id;
-        },
         'group' => $faker->word,
         'key' => $faker->word,
         'value' => $faker->sentence,
@@ -28,9 +22,6 @@ $factory->state(Translation::class, 'group', function (Generator $faker) {
 
 $factory->state(Translation::class, 'single', function (Generator $faker) {
     return [
-        'language_id' => function () {
-            return factory(Language::class)->create()->id;
-        },
         'group' => 'single',
         'key' => $faker->word,
         'value' => $faker->sentence,
