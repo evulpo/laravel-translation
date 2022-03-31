a2825aea99997d018364c71fa0e776ea8667c90b
GeorgBaseltETH <55834338+GeorgBaseltETH@users.noreply.github.com>
2022-03-31 10:27:31 +0200

    changed driver back to file

--- config/translation.php
+++ config/translation.php
@@ -11,7 +11,7 @@ return [
     | Supported: "file", "database"
     |
     */
-    'driver' => env('TMS_DRIVER'),
+    'driver' => "file",
 
     /*
     |--------------------------------------------------------------------------
