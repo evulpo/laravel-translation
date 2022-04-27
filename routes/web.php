327cafaa949381b98b496b8e7762cbd8e2d0b1fe
georg@evulpo.com <55834338+GeorgBaseltETH@users.noreply.github.com>
2022-04-27 10:29:16 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,24 @@
 <?php
 
 Route::group(config('translation.route_group_config') + ['namespace' => 'JoeDixon\\Translation\\Http\\Controllers'], function ($router) {
-    $router->get(config('translation.ui_url'), 'LanguageController@index')
+    $router->get('/', 'LanguageController@index')
         ->name('languages.index');
 
-    $router->get(config('translation.ui_url').'/create', 'LanguageController@create')
+    $router->get('/create', 'LanguageController@create')
         ->name('languages.create');
 
-    $router->post(config('translation.ui_url'), 'LanguageController@store')
+    $router->post('/', 'LanguageController@store')
         ->name('languages.store');
 
-    $router->get(config('translation.ui_url').'/{language}/translations', 'LanguageTranslationController@index')
+    $router->get('/{language}/translations', 'LanguageTranslationController@index')
         ->name('languages.translations.index');
 
-    $router->post(config('translation.ui_url').'/{language}', 'LanguageTranslationController@update')
+    $router->post('/{language}', 'LanguageTranslationController@update')
         ->name('languages.translations.update');
 
-    $router->get(config('translation.ui_url').'/{language}/translations/create', 'LanguageTranslationController@create')
+    $router->get('/{language}/translations/create', 'LanguageTranslationController@create')
         ->name('languages.translations.create');
 
-    $router->post(config('translation.ui_url').'/{language}/translations', 'LanguageTranslationController@store')
+    $router->post('/{language}/translations', 'LanguageTranslationController@store')
         ->name('languages.translations.store');
 });
