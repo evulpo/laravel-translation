ca45e579421d0cdf690c903bea3ff5bc80341b3a
georg@evulpo.com <55834338+GeorgBaseltETH@users.noreply.github.com>
2022-04-27 14:36:31 +0200

    Update Database.php

--- src/Drivers/Database.php
+++ src/Drivers/Database.php
@@ -2,6 +2,7 @@
 
 namespace JoeDixon\Translation\Drivers;
 
+use App\Jobs\SyncGlobalLanguages;
 use App\Jobs\SyncGlobalDB;
 use Illuminate\Support\Collection;
 use JoeDixon\Translation\Exceptions\LanguageExistsException;
