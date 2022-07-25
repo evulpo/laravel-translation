9001f76be5e338bdd1b8599db3df3bbe1c439cf5
NevenaR <nevena.radovano@gmail.com>
2022-07-25 18:27:30 +0200

    caching translations

--- src/Translation.php
+++ src/Translation.php
@@ -3,9 +3,14 @@
 namespace JoeDixon\Translation;
 
 use Illuminate\Database\Eloquent\Model;
+use Rennokki\QueryCache\Traits\QueryCacheable;
 
 class Translation extends Model
 {
+    use QueryCacheable;
+
+    protected static $flushCacheOnUpdate = true;
+
     protected $guarded = [];
 
     public function __construct(array $attributes = [])
@@ -15,6 +20,26 @@ class Translation extends Model
         $this->table = config('translation.database.translations_table');
     }
 
+    protected function cacheForValue()
+    {
+        return config('translation.cache_options.ttl');
+    }
+
+    protected function cacheTagsValue()
+    {
+        return config('translation.cache_options.tags');
+    }
+
+    protected function cachePrefixValue()
+    {
+        return  config('translation.cache_options.prefix');
+    }
+
+    protected function cacheDriverValue()
+    {
+        return config('translation.cache_options.driver');
+    }
+    
     public function language()
     {
         return $this->belongsTo(Language::class);
