953a7d5cecde168d659e62e86192d236a9c931d1
Saloni Kumawat <545saloni@gmail.com>
2024-02-08 21:53:37 +0100

    update form route

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,8 @@ Route::group(config('translation.route_group_config') + ['namespace' => 'JoeDixo
     $router->get(config('translation.ui_url').'/{language}/translations/create', 'LanguageTranslationController@create')
         ->name('languages.translations.create');
 
-    $router->post(config('translation.ui_url').'/{language}/translations', 'LanguageTranslationController@store')
-        ->name('languages.translations.store');
+    $router->get(config('translation.ui_url').'/{language}/translations/{group}/{key}/update', 'LanguageTranslationController@updateTranslation')
+        ->name('languages.translations.updateTranslation');
 
     $router->post(config('translation.ui_url').'/{language}/translations', 'LanguageTranslationController@storeAll')
         ->name('languages.translations.storeAll');
